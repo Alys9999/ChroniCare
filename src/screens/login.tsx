2d0af2e567fc8249557f5b1781353875835c35a4
AlyssaLu <41v1rj@gmail.com>
2024-01-05 18:53:34 -0800

    login layout setted up

--- src/screens/login.tsx
+++ src/screens/login.tsx
@@ -5,7 +5,8 @@ import {
   SafeAreaView,
   StyleSheet,
   Dimensions,
-  View
+  View,
+  Button
 } from 'react-native';
 import { TextInput } from 'react-native-gesture-handler';
 
@@ -17,12 +18,16 @@ function Login(): React.JSX.Element {
 
     return (
       <SafeAreaView style={{width: screenWidth, height: screenHeight}}>
-        <View>
+        <View style={{height: screenHeight*0.5, justifyContent: 'center', alignItems: 'center'}}>
         <Image source={require('../assets/chroniLogo.png')} style={styles.ratioLogo}></Image>
         </View>
         <View>
-            <TextInput></TextInput>
-            <TextInput></TextInput>
+            <TextInput placeholder='Enter Email'></TextInput>
+            <TextInput placeholder='Enter Password'></TextInput>
+        </View>
+        <View style={{gap: 2, marginHorizontal:3}}>
+          <Button onPress={()=>{}} title="Login"></Button>
+          <Button onPress={()=>{}} title="Sign up"></Button>
         </View>
         
       </SafeAreaView>
